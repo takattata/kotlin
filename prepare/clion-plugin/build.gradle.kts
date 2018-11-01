1ea4a0a7a2d717f4c32605cdeaab40f6bc51faed
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2018-11-01 17:07:20 +0300

    Use fixed versions for releases

--- prepare/clion-plugin/build.gradle.kts
+++ prepare/clion-plugin/build.gradle.kts
@@ -67,7 +67,7 @@ val preparePluginXml by task<Copy> {
 
     val sinceBuild =
         if (clionVersion.matches(Regex("\\d+\\.\\d+"))) clionVersion else clionVersion.substring(0, clionVersion.lastIndexOf('.'))
-    val untilBuild = clionVersion.substring(0, clionVersion.indexOf('.')) + ".*"
+    val untilBuild = clionVersion.substring(0, clionVersion.lastIndexOf('.')) + ".*"
 
     filter {
         it.replace("<!--version_placeholder-->",
